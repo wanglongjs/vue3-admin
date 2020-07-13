d352dd7a0cc34ff7797ed0633eec924750a71fcf
“X <daiwanglong1@163.com>
2020-07-13 22:31:52 +0800

    新页面提交

---
+++ vue/src/views/login/login.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="cotnent">
+    <div class="login">
+      <el-input placeholder="请输入内容" v-model="input10" clearable></el-input>
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  data() {
+    return {
+        input10:''
+    };
+  }
+};
+</script>
+
+<style lang="scss" scoped>
+.login {
+  background-color: rgb(7, 88, 88);
+  height: 100vh; //可视区窗口比例来处理
+}
+</style>
